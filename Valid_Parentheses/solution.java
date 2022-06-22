79acc44ce38de3bbb21abba4dcb037c6d21e281d
Mason Kleinsorge <Kleinsorgem97@gmail.com>
2022-06-22 17:10:35 -0500

    Add sick sln from leetcode user phoenix13steve

--- Valid_Parentheses/solution.java
+++ Valid_Parentheses/solution.java
@@ -20,4 +20,24 @@ public class solution {
         }
         return stack.empty();
     }
+
+    /**
+     * NOTE: Super sick solution from leetcode user phoenix13steve.
+     * https://leetcode.com/problems/valid-parentheses/discuss/9178/Short-java-solution
+     */
+
+    // public boolean isValid(String s) {
+    //     Stack<Character> stack = new Stack<Character>();
+    //     for (char c : s.toCharArray()) {
+    //         if (c == '(')
+    //             stack.push(')');
+    //         else if (c == '{')
+    //             stack.push('}');
+    //         else if (c == '[')
+    //             stack.push(']');
+    //         else if (stack.isEmpty() || stack.pop() != c)
+    //             return false;
+    //     }
+    //     return stack.isEmpty();
+    // }
 }

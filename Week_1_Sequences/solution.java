bb2b79690af63ce7392b658401468868efd940b3
Mason Kleinsorge <kleinsorgem97@gmail.com>
2022-07-28 13:59:23 -0500

    refactor threeSum

--- Week_1_Sequences/solution.java
+++ Week_1_Sequences/solution.java
@@ -106,29 +106,7 @@ public class solution {
 
         // 15. 3Sum: https://leetcode.com/problems/3sum/
         public List<List<Integer>> threeSum(int[] nums) {
-            List<List<Integer>> res = new ArrayList<>();
-            Arrays.sort(nums);
-            for (int i = 0; i < nums.length - 2; i++) {
-                if (nums[i] > 0)
-                    break;
-                if (i > 0 && nums[i] == nums[i - 1])
-                    continue;
-                int lo = i + 1, hi = nums.length - 1;
-                int target = -nums[i];
-                while (lo < hi) {
-                    if (nums[lo] + nums[hi] == target) {
-                        res.add(Arrays.asList(nums[i], nums[lo++], nums[hi--]));
-                        while (lo < hi && nums[lo] == nums[lo - 1])
-                            lo++;
-                        while (lo < hi && nums[hi] == nums[hi + 1])
-                            hi--;
-                    } else if (nums[lo] + nums[hi] < target)
-                        lo++;
-                    else
-                        hi--;
-                }
-            }
-            return res;
+		List<List<Integer>> res = ArrayList<>();
         }
 
 }

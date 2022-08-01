c3b52ca2105e1cc8b700cb1cd6060bb3432c2298
Mason Kleinsorge <Kleinsorgem97@gmail.com>
2022-08-01 15:53:11 -0500

    153. Find minimum in rotated sorted array

--- Week_2_Data_Structures/solution.java
+++ Week_2_Data_Structures/solution.java
@@ -58,4 +58,18 @@ public class solution {
         return maxVolume;
     }
 
+    // 153. Find Minimum in Rotated Sorted Array: https://leetcode.com/problems/find-minimum-in-rotated-sorted-array/
+    public int findMin(int[] nums) {
+        if (nums.length == 1) return nums[0];
+        int lo = 0, hi = nums.length - 1;
+        while (lo < hi) {
+            int mid = (lo + hi) / 2;
+            if (nums[mid] > nums[hi])
+                lo = mid + 1;
+            else
+                hi = mid;
+        }
+        return nums[lo];
+    }
+    
 }
